c6ec73796d3ebd72f129d9f4b3252d8e9328f67b
Bari Luis Baez Vasquez <1120873@est.intec.edu.do>
2024-10-18 02:45:06 -0400

    Add files via upload

---
+++ Leccion 1/Leccion 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DobleVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            numero1 = 45.5;
+            numero2 = 54.5;
+            double suma = numero1 + numero2;
+            Console.WriteLine("la suma de las dos variables es:" + suma);
+            Console.ReadKey();
+        }
+    }
+}
